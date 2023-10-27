059fef0a9db82bbf4f0acee6726fc5da12a98311
zeroincombenze <info@shs-av.com>
2023-10-27 15:27:00 +0200

    [IMP] Removed old unused modules

--- sale_force_invoiced/__manifest__.py
+++ sale_force_invoiced/__manifest__.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 {
-    'name': 'Sale Force Invoiced',
-    'summary': 'Allows to force the invoice status of the sales order to '
-               'Invoiced',
-    'version': '10.0.1.0.0',
-    "author": "Eficent,"
-              "Odoo Community Association (OCA)",
-    'category': 'sale',
-    'license': 'AGPL-3',
-    'website': "https://github.com/OCA/sale-workflow",
-    'depends': ['sale'],
-    'data': [
-        'view/sale_view.xml'
-    ],
-    'installable': True,
+    "name": "Sale Force Invoiced",
+    "version": "10.0.1.0.0",
+    "category": "sale",
+    "summary": "Allows to force the invoice status of the sales order to Invoiced",
+    "author": "Eficent, SHS-AV s.r.l., Odoo Community Association (OCA)",
+    "website": "https://www.zeroincombenze.it",
+    "development_status": "Beta",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ['view/sale_view.xml'],
+    "installable": True,
 }
